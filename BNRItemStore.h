e4ce0a48d1cfdb61002120e768217197e4c14e5f
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-09 14:44:32 +0530

    Created subclass BNRItemStore

---
+++ BNRItemStore.h
@@ -0,0 +1,15 @@
+//
+//  BNRItemStore.h
+//  Homepwner
+//
+//  Created by Guwanjith Tennekoon on 9/9/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItemStore : NSObject
+
++(instancetype)sharedStore;
+
+@end
---
+++ BNRItemStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemStore.m
+//  Homepwner
+//
+//  Created by Guwanjith Tennekoon on 9/9/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+
+@implementation BNRItemStore
+
+@end
