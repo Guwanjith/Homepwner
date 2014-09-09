d37586db03145950adf592fa1748b75b401854a6
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-09 14:26:10 +0530

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Guwanjith Tennekoon on 9/9/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
