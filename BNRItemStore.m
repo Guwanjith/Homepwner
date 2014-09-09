ce1e5750696564b37d1e870bd881d70f3831a12d
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-09 14:49:37 +0530

    Ensured initializers for BNRItemStore work as needed

--- BNRItemStore.m
+++ BNRItemStore.m
@@ -10,4 +10,31 @@
 
 @implementation BNRItemStore
 
++(instancetype)sharedStore
+{
+    static BNRItemStore *sharedStore;
+    
+    //Do i need to create a sharedStore?
+    if (!sharedStore) {
+        sharedStore = [[self alloc] initPrivate];
+    }
+    
+    return sharedStore;
+}
+
+//Call an error if someone tries to use [[BNRItemStore alloc] init]
+-(instancetype)init
+{
+    [NSException raise:@"Singleton"
+                format:@"Use +[BNRItemStore sharedStore]"];
+    return nil;
+}
+
+//Here is the real (secret) initializer
+-(instancetype)initPrivate
+{
+    self = [super init];
+    return self;
+}
+
 @end
