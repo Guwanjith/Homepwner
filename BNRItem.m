e49c542c2abccd3bf40c9cf1032f7e388d77d110
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-09 14:40:32 +0530

    Added in BNRItems from the radnomItems project

---
+++ BNRItem.m
@@ -0,0 +1,100 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Guwanjith Tennekoon on 8/30/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
++(instancetype)randomItem
+{
+    //Create an immutable array of three adjectives
+    NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
+    
+    //Create an immutable array of three nouns
+    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+    
+    //Get the index of a random adjective/noun from the lists
+    //Note: the % operator, called the modullo operator, gives you the remainder. so adjectiveIndex is a random number from 0 to 2 inclusive.
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    
+    //Note that NSInteger is not an object, but a type definition for "long"
+    NSString *randomName = [NSString stringWithFormat:@"%@ %@", [randomAdjectiveList objectAtIndex:adjectiveIndex], [randomNounList objectAtIndex:nounIndex]];
+    
+    int randomValue = arc4random() %100;
+    
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    'O' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    'O' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    'O' + arc4random() % 10];
+    
+    BNRItem *newItem = [[self alloc] initWithItemName:randomName
+                                       valueInDollars:randomValue
+                                         serialNumber:randomSerialNumber];
+    return newItem;
+}
+
+-(instancetype)initWithItemName:(NSString *)name
+                 valueInDollars:(int)value
+                   serialNumber:(NSString *)sNumber
+{
+ 
+    //Call the superclass's designated intializer
+    self = [super init];
+    
+    //Did the superclass's designated initializer succeed?
+    if(self){
+        //Give the instance variables initial values
+        _itemName = name;
+        _serialNumber = sNumber;
+        _valueInDollars = value;
+        //set _dateCreated to the current date and time
+        _dateCreated = [[NSDate alloc] init];
+    }
+    
+    //Return the address of the newly initialized object
+    return self;
+}
+
+-(instancetype)initWithItemName:(NSString *)name
+{
+    return [self initWithItemName:name
+                   valueInDollars:0
+                     serialNumber:@""];
+}
+
+-(instancetype)init
+{
+    return [self initWithItemName:@"Item"];
+}
+
+-(NSString *)description
+{
+    NSString *descriptionString = [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@", self.itemName, self.serialNumber, self.valueInDollars, self.dateCreated];
+    return descriptionString;
+}
+
+-(void)setItemName:(NSString *)itemName
+{
+    _itemName = [itemName copy];
+}
+
+-(void)setContainedItem:(BNRItem *)containedItem
+{
+    _containedItem = containedItem;
+    self.containedItem.container = self;
+}
+
+-(void)dealloc
+{
+    NSLog(@"Destroyed: %@", self);
+}
+
+@end
