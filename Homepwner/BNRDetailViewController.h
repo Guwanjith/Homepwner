f8374889f2d544b9762f504c5dc242ecb64c9ed4
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-11 14:36:09 +0530

    Created BNRDetailViewController

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Guwanjith Tennekoon on 9/11/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/BNRDetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by Guwanjith Tennekoon on 9/11/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
